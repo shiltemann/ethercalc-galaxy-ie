3eeb2a3cf5800ca1a40599af10036b7405394dc7
Saskia Hiltemann <zazkia@gmail.com>
2017-02-02 00:18:02 +0100

    nicer file naming and handle empty audit trail

--- Docker/galaxy_export.py
+++ Docker/galaxy_export.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import os
 import csv
 import json
 
@@ -11,21 +11,30 @@ def csv_to_tsv(infile, outfile):
         csv.writer(fout, delimiter="\t").writerows(csv.reader(fin))
 
 
-def get_audit_trail():
-    with open('dump.json') as d, open('ethercalc_audit', 'w+') as fout:
-        j = json.load(d)
-        audit = j.get('audit-galaxy')
-        for command in audit:
-            fout.write(command.encode('UTF8') + '\n')
+def get_audit_trail(audit_file):
+    try:
+        with open('dump.json') as d, open(audit_file, 'w+') as fout:
+            j = json.load(d)
+            audit = j.get('audit-galaxy')
+            for command in audit:
+                fout.write(command.encode('UTF8') + '\n')
+    except:
+        with open(audit_file, 'w+') as fout:
+            fout.write("no edits were made to your dataset")
 
 
 if __name__ == "__main__":
+    # get info on import dataset for naming
+    hid = os.environ['DATASET_HID']
+    audit_file = 'ethercalc audit trail for dataset '+hid
+    export_file = 'ethercalc export on dataset '+hid
+
     # send worksheet to galaxy history
-    csv_to_tsv('ethercalc_saved', 'ethercalc_export')
+    csv_to_tsv('ethercalc_saved', export_file)
 
     # get audit trail of worksheet, output one command per line
-    get_audit_trail()
+    get_audit_trail(audit_file)
 
     # send outputs to Galaxy
-    put('ethercalc_audit', file_type='txt')
-    put('ethercalc_export', file_type='tabular')
+    put(audit_file, file_type='txt')
+    put(export_file, file_type='tabular')
